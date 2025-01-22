c4dfb382cb9a3b64cf481f263ce32e6709700b94
Tuhin Pramanik <156042548+TuhinPramanik4@users.noreply.github.com>
2025-01-22 20:46:48 +0530

    initialize the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+function App() {
+  return (
+      <div className='w-full h-screen bg-zinc-600 text-white'>
+         <Navbar />
+      </div>
+  )
+}
+
+export default App
