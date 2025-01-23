d0a6f8a13cb78e20d1a4e13644772b68a82a4782
Tuhin Pramanik <156042548+TuhinPramanik4@users.noreply.github.com>
2025-01-23 20:34:27 +0530

    Create Moving Eyes Feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar'
 import LandingPage from './Components/LandingPage'
 import Marquee from './Components/Marquee'
 import About from './Components/About'
+import Eyes from './Components/Eyes'
 function App() {
   return (
       <div className='w-full min-h-screen bg-zinc-900 text-white'>
@@ -10,6 +11,7 @@ function App() {
          <LandingPage />
          <Marquee />
          <About />
+         <Eyes />
       </div>
   )
 }
